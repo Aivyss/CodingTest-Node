5d09a4eefc42f982b6b73d1beaed2697dfcca73e
Aivyss <hklee6417@gmail.com>
2021-10-13 16:49:59 +0900

    amend: prgrammers -77484

--- programmers/77484.js
+++ programmers/77484.js
@@ -3,12 +3,14 @@
 
 function solution(lottos, win_nums) {
     let cnt = 0;
-    let zeroCnt = lottos.filter((curr) => curr === 0).length;
+    let zeroCnt = lottos.filter((curr) => !curr).length;
 
-    win_nums.forEach((winNum) => (cnt += lottos.filter((lotNum) => lotNum === winNum).length));
+    win_nums.forEach((winNum) => (cnt += lottos.some((lotNum) => lotNum === winNum)));
 
     return [
         Math.abs(cnt + zeroCnt - 7) >= 6 ? 6 : Math.abs(cnt + zeroCnt - 7),
         Math.abs(cnt - 7) >= 6 ? 6 : Math.abs(cnt - 7),
     ];
 }
+
+console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]));
