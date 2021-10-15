dce615682e6f0cfa343a8ad32b25cac68fb4eb48
Aivyss <hklee6417@gmail.com>
2021-10-16 04:52:49 +0900

    amend: programmer-70128

--- programmers/70128.js
+++ programmers/70128.js
@@ -1,6 +1,4 @@
 // https://programmers.co.kr/learn/courses/30/lessons/70128
 // Author: Aivyss
 
-function solution(a, b) {
-    return a.reduce((prev, curr, idx) => prev + curr * b[idx], 0);
-}
+const solution = (a, b) => a.reduce((prev, curr, idx) => prev + curr * b[idx], 0);
